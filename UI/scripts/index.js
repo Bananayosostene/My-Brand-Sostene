a48e263574aa1198866c8af61e451688709f5992
Bananayosostene <sbananayo98@gmail.com>
2024-02-14 18:58:16 +0200

    humberger menu

--- UI/scripts/index.js
+++ UI/scripts/index.js
@@ -1,33 +1,48 @@
 function validation() {
   let name = document.getElementById("name").value;
   let email = document.getElementById("mail").value;
-    let textarea = document.getElementById("textarea").value;
+  let textarea = document.getElementById("textarea").value;
+ 
     
-  if (name.trim() === "") {
-    document.getElementById("name-p").innerHTML = "Please enter your name";
-    return false;
-  }
-
-  if (email.trim() === "") {
-    document.getElementById("email-p").innerHTML = "Please enter your Email";
-    return false;
+    if (true) {
+        
+        if (name.trim() === "") {
+          document.getElementById("name-p").innerHTML = "Please enter your name";
+          return false;
+        }
+        else {
+            document.getElementById("name-p").innerHTML = " ";
+          }
+      
+        if (email.trim() === "") {
+          document.getElementById("email-p").innerHTML = "Please enter your Email";
+          return false;
+          }
+        else {
+            document.getElementById("email-p").innerHTML = " ";
+          }
+      
+        if (!validateEmail(email)) {
+          document.getElementById("email-val-p").innerHTML = "Invalid email format";
+          return false;
+        }
+        else {
+            document.getElementById("email-val-p").innerHTML = " ";
+          }
+      
+        if (textarea.trim() === "") {
+          document.getElementById("textarea-p").innerHTML = "Write comment";
+          return false;
+        }
+       else {
+            document.getElementById("textarea-p").innerHTML = " ";
+          }
     }
-  else {
-      document.getElementById("email-p").innerHTML = " ";
-    }
+        
+        document.getElementById("sms-sent").innerHTML = `Message sent successfully !`;
+        return true;
+    
 
-  if (!validateEmail(email)) {
-    document.getElementById("email-val-p").innerHTML = "Invalid email format";
-    return false;
-  }
-
-  if (textarea.trim() === "") {
-    document.getElementById("textarea-p").innerHTML = "Write comment";
-    return false;
-  }
-
-  document.getElementById("success").innerHTML = "Message sent successfully!";
-  return true;
 }
 
  
