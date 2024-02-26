f9aa0a00f907ca8209e3cd5cf3f4baaab92167c2
Bananayosostene <sbananayo98@gmail.com>
2024-02-26 08:58:41 +0200

    admin login

--- UI/scripts/login.js
+++ UI/scripts/login.js
@@ -22,18 +22,33 @@ function login() {
 
   const users = getUsers();
   let isRegistered = false;
+  let isAdmin = true;
 
+  isAdmin = users.find((user) => {
+    return user.pass == pass && user.name == names && user.role == "admin";
+  });
+
+  
   isRegistered = users.find((user) => {
     return user.pass == pass && user.name == names;
   });
 
-  if (isRegistered) {
+  
+  if (isAdmin && isRegistered) {
     // alert("Login success");
     localStorage.setItem("loggedUser", JSON.stringify(isRegistered));
     // console.log(isRegistered);
     window.location.href = "dashboard.html";
-    
-  } else {
-    alert("User not found, signup first");
+  }
+
+   
+  else if (isRegistered) {
+    // alert("Login success");
+    localStorage.setItem("loggedUser", JSON.stringify(isRegistered));
+    // console.log(isRegistered);
+    window.location.href = "../index.html";
+  }
+  else {
+    alert("User not found, please sign up!")
   }
 }
