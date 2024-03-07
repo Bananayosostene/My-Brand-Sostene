42a6ced07f0b2bc253e9a27e77952a05bd9b2d49
Bananayosostene <sbananayo98@gmail.com>
2024-03-07 11:48:46 +0200

    get blogs

--- UI/scripts/homeblogs.js
+++ UI/scripts/homeblogs.js
@@ -3,32 +3,63 @@
 
 function getBlogs() {
   const blogsString = localStorage.getItem("blogs");
+  
   return blogsString ? JSON.parse(blogsString) : [];
 }
 
+let moreblogs = getBlogs()
+console.log("moreblogs", moreblogs)
+
 function redirectSingleBlog(id) {
   // console.log(id);
-  window.location.href = `pages/moreblogs.html?blogId=${id}`;
+  window.location.href = `pages/moreblogs.html?blogId=${blog._id}`;
   console.log(id);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var moreBlogs = getBlogs();
-  moreBlogs.map((blog, index) => {
-    const allblog = document.getElementById("allblog");
-    allblog.innerHTML += `
+const fillBlogs = (blogs) => {
+  const slicedBlogs = blogs.slice(0, 3);
+  
+     slicedBlogs.map((blog, index) => {
+       const allblog = document.getElementById("allblog");
+       allblog.innerHTML += `
 
             <div >
-                <div><img class="portfolio-imgs" src="${blog.blogImg}"/></div>
+                <div><img class="portfolio-imgs" src="${blog.image}"/></div>
                 <div class="sdisc">
                     <h3>${blog.title}</h3>
-                    <p>${blog.blogDisc.slice(0, 70)} .....</p>
+                    <p>${blog.description}</p>
                 </div>
-                <input type="button" value="READ MORE" id="submit" class="sending" onclick="redirectSingleBlog(${
-                  blog.id
-                })"> <span id="success"></span>
+                <input type="button" value="READ MORE" id="submit" class="sending" onclick="redirectSingleBlog(${blog.id})"> <span id="success"></span>
             </div>
           
         `;
+     });
+  }
+
+
+
+
+const apiUrl = "http://localhost:5000/brand/blog/gets";
+
+// Make a GET request using the Fetch API
+fetch(apiUrl, {
+  method: "GET",
+
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(userData => {
+    // Process the retrieved user data
+    console.log('User Data:', userData.data);
+    fillBlogs(userData.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
-});
+console.log("fetch here")
+
+// fillBlogs(moreblogs);
